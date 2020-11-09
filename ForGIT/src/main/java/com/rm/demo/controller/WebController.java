ddcfd04bffc67e75111d43b144535f53547dea2d
mani <rangamani.ravuri@dxc.com>
2020-11-09 12:21:51 +0530

    changed mapping url from hi to hello

---
+++ ForGIT/src/main/java/com/rm/demo/controller/WebController.java
@@ -0,0 +1,19 @@
+package com.rm.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello world";
+	}
+	
+	
+	
+	
+	
+	
+	
+}
